e0b792f5a89535a2e66c1db640059c50f7d87f4c
Huy Tran Tuan <trantuanhuyltv@gmail.com>
2025-06-09 20:51:42 +0700

    Update splice, slice

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+let fruits2 = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
 const twoDimensionalArray = [
     [1, 2, 3],
     [4, 5, 6],
@@ -16,101 +17,139 @@ const people = [
 
 //splice
 Array.prototype.splice2 = function(start, deleteCount, ...items) {
-    const arr = this;
+    let length = this.length;
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
+    const copyArray = [];
+    for (let i = 0; i < length; i++) {
         if (i >= start && i < start + deleteCount) {
-            result.push(arr[i]);
+            result.push(this[i]);
+        }
+    }
+    for(let i=0; i<=start; i++){
+        copyArray.push(this[i])
+    }
+    if(items)
+        for(let i=0; i<items.length; i++){
+            copyArray.push(items[i])
+        }
+    for(let i=start + deleteCount; i<length; i++){
+        copyArray.push(this[i])
+    }
+    fruits2 = copyArray
+    return result;
+}
+// console.log("* Splice 2")
+// console.log(fruits2.splice2(1, 2, "Lemon", "Apple", "Mango")); //
+
+
+
+//slice
+Array.prototype.slice2 = function(start, end) {
+    if(start > end) return "False input!"
+    let length = this.length;
+    const result = [];
+    if(start < 0){
+        for(let i = length + start; i< length; i++){
+            result.push(this[i]);
+        }
+        for(let i = 0; i< end; i++){
+            result.push(this[i]);
+        }
+    }
+    else{
+        for (let i = start; i < end; i++) {
+            result.push(this[i]);
         }
     }
     return result;
 }
-console.log(fruits.splice2(1, 2, "Lemon", "Apple", "Mango"));
+// console.log("\n* Slice 2")
+// console.log(fruits.slice2(1, 3));
+// console.log(fruits.slice2(-2, 3));
+// console.log(fruits.slice2(3, 4));
 
-//slice
-Array.prototype.slice2 = function(start, end) {
-    const arr = this;
-    const result = [];
-    for (let i = start; i < end; i++) {
-        result.push(arr[i]);
-    }
-    return result;
-}
-console.log(fruits.slice2(1, 3));
 
 //sort
 Array.prototype.sort2 = function(compareFunction) {
-    const arr = this;
+    let length = this.length;
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i]);
+    for (let i = 0; i < length; i++) {
+        result.push(this[i]);
     }
-    return result.sort(compareFunction);
+    return result
 }
-console.log(people.sort2((a, b) => a.age - b.age));
+// console.log("\n* Sort 2");
+// console.log(people.sort2((a, b) => a.age - b.age));
 
 //reverse
 Array.prototype.reverse2 = function() {
-    const arr = this;
+    let length = this.length;
     const result = [];
-    for (let i = arr.length - 1; i >= 0; i--) {
-        result.push(arr[i]);
+    for (let i = length - 1; i >= 0; i--) {
+        result.push(this[i]);
     }
     return result;
 }
-console.log(people.reverse2());
+// console.log("\n* Reverse 2")
+// console.log(people.reverse2());
 
 //concat
-Array.prototype.concat2 = function(...items) {
-    const arr = this;
+Array.prototype.concat2 = function(param) {
+    let length = this.length;
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i]);
+    for (let i = 0; i < length; i++) {
+        result.push(this[i]);
     }
-    return result.concat(...items);
+    // if()
+    return result
 }
-console.log(fruits.concat2("Banana", "Orange", "Lemon", "Apple", "Mango"));
+// console.log("\n* Concat 2")
+// console.log(fruits.concat2("Banana", "Orange", "Lemon", "Apple", "Mango"));
 
 //join
 Array.prototype.join2 = function(separator) {
-    const arr = this;
+    let length = this.length;
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i]);
+    for (let i = 0; i < length; i++) {
+        result.push(this[i]);
     }
-    return result.join(separator);
+    return result
 }
-console.log(fruits.join2(","));
+// console.log("\n* Join 2")
+// console.log(fruits.join2(","));
 
 //flat
 Array.prototype.flat2 = function() {
-    const arr = this;
+    let length = this.length;
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i]);
+    for (let i = 0; i < length; i++) {
+        result.push(this[i]);
     }
-    return result.flat();
+    return result
 }
-console.log(twoDimensionalArray.flat2());
+// console.log("\n* Flat 2")
+// console.log(twoDimensionalArray.flat2());
 
 //filter
 Array.prototype.filter2 = function(callback) {
-    const arr = this;
+    let length = this.length;
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i]);
+    for (let i = 0; i < length; i++) {
+        result.push(this[i]);
     }
     return result.filter(callback);
 }
-console.log(people.filter2((person) => person.age > 20));
+// console.log("\n* Filter 2")
+// console.log(people.filter2((person) => person.age > 20));
 
 //reduce
 Array.prototype.reduce2 = function(callback, initialValue) {
-    const arr = this;
+    let length = this.length;
     const result = [];
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i]);
+    for (let i = 0; i < length; i++) {
+        result.push(this[i]);
     }
     return result;
 }
-console.log(people.reduce2((acc, person) => acc + person.age, 0));
+// console.log("\n* Reduce 2")
+// console.log(people.reduce2((acc, person) => acc + person.age, 0));
